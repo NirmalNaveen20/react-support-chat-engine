edca146e8c2c13aeb367f633f812fe676d2f6f34
Nirmal Naveen <53178630+NirmalNaveen20@users.noreply.github.com>
2022-12-06 23:49:23 +0530

    make changes to the support window

--- src/SupportEngine/index.js
+++ src/SupportEngine/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect, useState} from "react";
 import Avatar from './Avatar'
+import SupportWindow from "./SupportWindow";
 
 const SupportEngine = () => {
+    const ref = useRef(null)
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setVisible(false)
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
+
     return (
-        <div>
+        <div ref={ref}>
+            <SupportWindow
+                visible={visible}            
+            />
             <Avatar
+                onClick={() => setVisible(true)}
                 style={ {position: 'fixed', bottom: '24px', right: '24px'} }
              />
         </div>
